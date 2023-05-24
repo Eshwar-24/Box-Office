004d142c7faaf9be42f87b36d18cb2f04bdcc497
Eshwar_24 <saieshwarmanchala42@gmail.com>
2023-05-24 22:37:23 +0530

    Updates

---
+++ precache-manifest.1c86d31acf22ec5caea85b9d3ba2f0bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa09d521fdd82faca058",
+    "url": "/box-office/static/css/main.287504cc.chunk.css"
+  },
+  {
+    "revision": "aa09d521fdd82faca058",
+    "url": "/box-office/static/js/main.aa09d521.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/box-office/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "18b3725822c6f33446c7",
+    "url": "/box-office/static/js/2.18b37258.chunk.js"
+  },
+  {
+    "revision": "cab3bbf61fc32b6a7188",
+    "url": "/box-office/static/js/runtime~main.cab3bbf6.js"
+  },
+  {
+    "revision": "9c3c5c34fc9f0112c3170b2584fd5b62",
+    "url": "/box-office/static/media/404-error.9c3c5c34.jpg"
+  },
+  {
+    "revision": "8d15b6752c53ff831fa9e7fd4efd0b69",
+    "url": "/box-office/index.html"
+  }
+];
